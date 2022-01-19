7af61072174c32943bd2418c3c5ee12f0106d1ac
Iconer <yoyofa2@hotmail.com>
2022-01-20 04:02:13 +0900

    The game scene is modified.

--- Mole Hunter/Assets/Scripts/GameManager.cs
+++ Mole Hunter/Assets/Scripts/GameManager.cs
@@ -8,21 +8,42 @@ using ExPhoton = ExitGames.Client.Photon;
 
 public class GameManager : PunBehaviour
 {
+	public float Game_begin_time = 3.5f;
+	public float Game_time = 0f, Game_duration = (float)System.TimeSpan.FromMinutes(3).TotalSeconds;
 	public enum PHASE
 	{
-		READY, GAME, FADEOUT, DONE
+		READY, GAME, COMPLETE, DONE
 	}
-	public PHASE Phase = PHASE.READY;
-	public Camera Camera;
-	public float Camera_speed = 10f;
+	private PHASE __Phase = PHASE.READY;
+	public PHASE Phase
+	{
+		get
+		{
+			return __Phase;
+		}
+		set
+		{
+			if (__Phase != value)
+			{
+				print("The state of game is changed to " + value + " from " + __Phase);
+				__Phase = value;
+			}
+		}
+	}
+
+	public GameObject Camera;
+	public float Camera_speed = 3f;
 
 	public void Awake()
 	{
-		Camera = FindObjectOfType<Camera>();
+		Camera = GameObject.Find("GameCamera");
+		print("Camera is " + Camera.ToString());
 	}
 	public void Start()
 	{
+		Game_time = 0f;
 
+		StartCoroutine(GameProcess());
 	}
 	public void Update()
 	{
@@ -34,17 +55,45 @@ public class GameManager : PunBehaviour
 		switch (Phase)
 		{
 			case PHASE.READY:
-				{
+			{
 
-				}
-				Camera = Camera.main;
-				break;
+			}
+			break;
 
 			case PHASE.GAME:
-				break;
+			{
+				var deltas = Time.deltaTime;
+
+				var camvelocity = new Vector3(0f, Camera_speed, 0f);
+				this.Camera.transform.TransformVector(camvelocity);
+				this.Camera.transform.Translate(camvelocity * deltas);
+			}
+			break;
 
 			case PHASE.DONE:
-				break;
+			{
+
+			}
+			break;
 		}
+	}
+	public void OnGUI()
+	{
+		
+	}
+
+	private IEnumerator GameProcess()
+	{
+		yield return new WaitForSeconds(Game_begin_time);
+
+		Phase = PHASE.GAME;
+
+		while (Game_time < Game_duration)
+		{
+			Game_time += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+
+		Phase = PHASE.COMPLETE;
 	}
 }
